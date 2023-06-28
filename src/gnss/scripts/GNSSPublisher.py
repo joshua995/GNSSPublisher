098cba055e8cf203c33dd73040e52686161782b3
joshua995 <114888100+joshua995@users.noreply.github.com>
2023-06-27 20:45:05 -0600

    Update GNSSPublisher.py

--- src/gnss/scripts/GNSSPublisher.py
+++ src/gnss/scripts/GNSSPublisher.py
@@ -10,6 +10,7 @@ def talker():
     pub = rospy.Publisher('chatter', String, queue_size=10)
     rospy.init_node('talker', anonymous=True)
     rate = rospy.Rate(10) # 10hz
+    
     serialPort = serial.Serial(port = "/dev/ttyACM0", baudrate=38400, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
     
     ggaLat, ggaLon, ggaAlt = 0, 0, 0
@@ -28,8 +29,10 @@ def talker():
                 rmcLat, rmcLon = splitSerialString[3], splitSerialString[5]
             if serialString.__contains__(bytes("GLL", "utf-8")):
                 gllLat, gllLon = splitSerialString[1], splitSerialString[3]
+                
                 averageLat = (float(ggaLat) + float(gllLat) + float(rmcLat)) / 3
                 averageLon = (float(ggaLon) + float(gllLon) + float(rmcLon)) / 3
+                
                 latLonString = " " + str(averageLat) + "," + str(averageLon)
                 rospy.loginfo(latLonString)
                 pub.publish(latLonString)
